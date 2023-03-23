245e7a0ccf65aae91f9e31a477933768d173ca8d
xgervill <xgervill@host.docker.internal>
2023-03-23 14:54:42 +0100

    Avoid nulls and empty names in Person

--- demo/src/main/java/com/example/ejemplos/Persona.java
+++ demo/src/main/java/com/example/ejemplos/Persona.java
@@ -1,12 +1,60 @@
 package com.example.ejemplos;
 
+import java.util.Optional;
+
+import com.example.exceptions.InvalidArgumentException;
+
 import lombok.Builder;
 import lombok.Data;
 
-@Data
+
 @Builder
 public class Persona {
 	private int id;
 	private String nombre;
 	private String apellidos;
+	
+	public Persona(int id, String nombre, String apellidos) {
+		super();
+		this.id = id;
+		setNombre(nombre);
+		setApellidos(apellidos);
+	}
+	
+	public Persona(int id, String nombre) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		if (nombre == null || nombre == "" || nombre.length() < 2) throw new IllegalArgumentException();
+		this.nombre = nombre;
+	}
+
+	public Optional<String> getApellidos() {
+		return Optional.ofNullable(this.apellidos);
+	}
+
+	public void setApellidos(String apellidos) {
+		if (apellidos == null || "".equals(apellidos) || apellidos.length() < 2) throw new IllegalArgumentException();
+		this.apellidos = apellidos;
+	}
+	
+	public void removeApellidos(String apellidos) {
+		this.apellidos = null;
+	}
+	
 }
