a011836e83f30988187d18c157125df3a16a65f0
xgervill <xgervill@host.docker.internal>
2023-03-23 11:13:31 +0100

    Added minor explaination

--- demo/src/test/java/com/example/ejemplos/PersonaTest.java
+++ demo/src/test/java/com/example/ejemplos/PersonaTest.java
@@ -10,9 +10,12 @@ import lombok.experimental.var;
 
 /*
  * 1- comprobar que se puede crear la clase
+ *  * Se dejan "sueltas" para identificar correctamente el fallo, si estas comprobaciones fallan el resto de comprobaciones no se pueden hacer/no tendrán resultados válidos o esperados
  * 		1.1- El objeto creado no es nulo (se ha creado "algo")
  * 		1.2- El obejto creado es del tipo que nos interesa (instance of)
- * 2- comprobar que la inicialización es correcta*/
+ * 2- comprobar que la inicialización es correcta
+ *  * Se pueden agrupar bajo un assertAll ya que si una comprobación falla el resto se pueden realizar, además de permitir identificar los errores de manera más rápida sin tener que repetir caso por caso
+*/
 
 class PersonaTest {
 
