cd0d3c36bcf89e95468a2c26dce283a2029697d1
xgervill <xgervill@host.docker.internal>
2023-03-27 11:51:15 +0200

    Context configuration

--- demo/src/test/java/com/example/DemoApplicationTests.java
+++ demo/src/test/java/com/example/DemoApplicationTests.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.ContextConfiguration;
 
@@ -24,7 +25,7 @@ class DemoApplicationTests {
 		assertEquals("Soy el StringRepositoryImpl", dao.load());
 	}
 	
-	//Creamos una clase de configuración
+	//Creamos una clase de configuración (mock o clase especial para modificar el comportamiento normal --> cambios entre test e integración por ejemplo)
 	public static class IoCTestConfig{
 		@Bean
 		StringRepository getServicio() {
@@ -32,8 +33,9 @@ class DemoApplicationTests {
 		}
 	}
 	
+	//Una misma clase se puede reutilizar varias veces mediante ContextConfiguration (si
 	
-	//con el contextConfiguration indicamos que queremos utilizar la clase creada en vez de la clase "que tocaría" (en este caso correspondería a StringRepositoryImpl
+	//con el contextConfiguration indicamos que queremos utilizar la clase creada en vez de la clase "que tocaría" (en este caso correspondería a StringRepositoryImpl)
 	@Nested
 	@ContextConfiguration(classes = IoCTestConfig.class)
 	class IoCTest {
@@ -45,5 +47,25 @@ class DemoApplicationTests {
 			assertEquals("Soy el doble de pruebas de StringRepository", dao.load());
 		}
 	}
-
+	
+	//Si en cambio se quiere utilizar la clase en un único sitio es mejor utilizar una configuración a nivel de test (no se puede reaprovechar la clase en varios tests)
+	
+	@Nested
+	class IoCUnicoTest {
+		@TestConfiguration
+		public static class IoCParticularTestConfig {
+			@Bean
+			StringRepository getServicio() {
+				return new StringRepositoryMockImpl();
+			}
+		}
+		
+		@Autowired
+		StringRepository dao;
+		
+		@Test
+		void contextLoads() {
+			assertEquals("Soy el doble de pruebas de StringRepository", dao.load());
+		}
+	}
 }
