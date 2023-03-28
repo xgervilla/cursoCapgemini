0b7b78f309140ba79cce676e91f021732d9f6649
xgervill <xgervill@host.docker.internal>
2023-03-28 14:09:50 +0200

    Example of queries with JPQL and SQL

--- demo/src/main/java/com/example/DemoApplication.java
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -3,6 +3,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.domain.Sort;
 
 import com.example.domains.contracts.repositories.ActorRepository;
 import com.example.domains.entities.Actor;
@@ -23,7 +24,14 @@ public class DemoApplication implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		System.out.println("Aplicación arrancada");
 		
-		dao.findTop5ByFirstNameStartingWithOrderByLastNameDesc("P").forEach(System.out::println);
+		//dao.findTop5ByFirstNameStartingWithOrderByLastNameDesc("P").forEach(System.out::println);
+		//dao.findTop5ByFirstNameStartingWith("P", Sort.by("firstName")).forEach(System.out::println);
+		
+		System.out.println("");
+		dao.findConNativeSQL(3).forEach(System.out::println);
+		
+		System.out.println("");
+		dao.findConJPQL(3).forEach(System.out::println);
 	}
 
 }
--- demo/src/main/java/com/example/domains/contracts/repositories/ActorRepository.java
+++ demo/src/main/java/com/example/domains/contracts/repositories/ActorRepository.java
@@ -1,10 +1,24 @@
 package com.example.domains.contracts.repositories;
 
 import java.util.List;
+import org.springframework.data.domain.Sort;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import com.example.domains.entities.Actor;
 
 public interface ActorRepository extends JpaRepository<Actor, Integer> {
 	List<Actor> findTop5ByFirstNameStartingWithOrderByLastNameDesc(String prefijo);
+	
+	List<Actor> findTop5ByFirstNameStartingWith(String prefijo, Sort order);
+	
+	@Query("SELECT a FROM Actor a WHERE a.actorId < ?1")
+	List<Actor> findConJPQL(int actorId);
+	
+	@Query(value = "SELECT * FROM actor a WHERE a.actor_id < :id", nativeQuery = true)
+	List<Actor> findConNativeSQL(@Param("id") int actorId);
+	
+	
 }
