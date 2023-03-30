0172e1708c706190c5a5606a54a7ad938d06b271
xgervilla <xavier.gervilla@gmail.com>
2023-03-30 08:52:09 +0200

    Base classes and interfaces

---
+++ catalogo/src/main/java/com/example/domains/contracts/repositories/CategoryRepository.java
@@ -0,0 +1,13 @@
+package com.example.domains.contracts.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import com.example.domains.core.repositories.contracts.RepositoryWithProjections;
+import com.example.domains.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Integer>, JpaSpecificationExecutor<Category>, RepositoryWithProjections{
+	<T> List<T> findAllBy(Class<T> type);
+}
