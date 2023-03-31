fc32d72b843ada22e3cdb9cb410e3580bc74f4c4
xgervilla <xavier.gervilla@gmail.com>
2023-03-31 09:32:46 +0200

    FirstName tests from Actor

--- catalogo/src/test/java/com/example/entities/ActorTest.java
+++ catalogo/src/test/java/com/example/entities/ActorTest.java
@@ -1,11 +1,13 @@
 package com.example.entities;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -17,20 +19,23 @@ public class ActorTest {
 		
 	}
 	
+	@Test
+	void testIsValid() {
+		var fixture = new Actor(0,"Pepito","GRILLO");
+		assertTrue(fixture.isValid());
+	}
 	
-	@Nested
-	@DisplayName("Personas correctas")
-	class OK{
-		@ParameterizedTest(name="actorId: {0}, firstName: {1}, lastName: {2}")
-		@CsvSource({"0,Juan,PALOMO", "-3,Pinto,POLLO"})
-		void testValid(int actorId, String firstName, String lastName) {
-			var a = new Actor(actorId, firstName, lastName);
-			
-			assertNotNull(a);
-			
-			assertTrue(a instanceof Actor, "No es instancia de actor");
-			
-			assertTrue(a.isValid());
-		}
+	
+	@DisplayName("size must be between 2 and 45 and must not be blank.")
+	@ParameterizedTest(name="FirstName: {0}, Error: {1}")
+	@CsvSource({"'','ERRORES: firstName: must not be blank, size must be between 2 and 45.'",
+		"' ','ERRORES: firstName: size must be between 2 and 45, must not be blank.'",
+		"'     ','ERRORES: firstName: must not be blank.'",
+		"A,'ERRORES: firstName: size must be between 2 and 45.'",
+		"12345678901234567890123456789012345678901234567890,'ERRORES: firstName: size must be between 2 and 45.'"})
+	void testNombreIsInvalid(String actorName, String error) {
+		var actor = new Actor(0, actorName, "GRILLO");
+		assertTrue(actor.isInvalid());
+		assertEquals(error, actor.getErrorsMessage());
 	}
 }
