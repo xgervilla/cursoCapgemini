cd9b5b7c0df4c5c20c936f400ba2fc453bfabb52
xgervilla <xavier.gervilla@gmail.com>
2023-04-02 17:37:55 +0200

    Name test on Language

--- catalogo/src/test/java/com/example/domains/entities/LanguageTest.java
+++ catalogo/src/test/java/com/example/domains/entities/LanguageTest.java
@@ -3,7 +3,19 @@ package com.example.domains.entities;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+/*
+ * Entidad Language:
+ 	*  languageId	--> siempre 0, dejamos a la BD la responsabilidad de asignar el ID que corresponda 
+ 	*  name			--> nombre del lenguaje: hasta un máximo de 20 carácteres, sin límite inferior PERO no puede ser blank ni nulo
+ 	*  last_update	--> "intocable", viene dado por la BD así que no se debe modificar 
+ 	*  films		--> lista de peliculas que tienen el lenguaje: sin restricciones en la construcción del objeto
+ 	*  filmsVO		--> lista de peliculas que tienen el lenguaje en VO: sin restricciones en la construcción del objeto
+*/
 
 class LanguageTest {
 
@@ -12,8 +24,21 @@ class LanguageTest {
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testIsValid() {
+		var item = new Language(0,"Spanish");
+		assertTrue(item.isValid());
+	}
+	
+	
+	@DisplayName("name invalid")
+	@ParameterizedTest(name="Name: {0}, Error: {1}")
+	@CsvSource({"'','ERRORES: name: must not be blank.'",
+		"'     ','ERRORES: name: must not be blank.'",
+		"12345678901234567890123456789012345678901234567890,'ERRORES: name: size must be between 0 and 20.'"})
+	void testNameIsInvalid(String languageName, String error) {
+		var language = new Language(0, languageName);
+		assertTrue(language.isInvalid());
+		assertEquals(error, language.getErrorsMessage());
 	}
 
 }
