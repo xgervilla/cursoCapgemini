a61c8e66eb876b4f01df7556710f63729916d4da
xgervilla <xavier.gervilla@gmail.com>
2023-04-03 11:58:34 +0200

    Created class Persona and PersonaDTO

---
+++ lotes/src/main/java/com/example/models/Persona.java
@@ -0,0 +1,14 @@
+package com.example.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Persona {
+
+	private long id;
+	private String nombre, correo, ip;
+}
---
+++ lotes/src/main/java/com/example/models/PersonaDTO.java
@@ -0,0 +1,13 @@
+package com.example.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PersonaDTO {
+	private long id;
+	private String nombre, apellidos, correo, sexo, ip;
+}
