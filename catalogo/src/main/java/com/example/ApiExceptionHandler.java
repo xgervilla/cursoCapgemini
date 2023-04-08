cbc30998dc881d78e1e418a66e8dd32a6fb1dd1b
xgervilla <xavier.gervilla@gmail.com>
2023-04-08 20:00:45 +0200

    Removed unused code

--- catalogo/src/main/java/com/example/ApiExceptionHandler.java
+++ catalogo/src/main/java/com/example/ApiExceptionHandler.java
@@ -4,35 +4,29 @@ import org.springframework.web.ErrorResponse;
 import org.springframework.web.ErrorResponseException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.server.ResponseStatusException;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
-import com.example.exceptions.BadRequestException;
-import com.example.exceptions.DuplicateKeyException;
-import com.example.exceptions.InvalidDataException;
-import com.example.exceptions.NotFoundException;
+import com.example.exceptions.*;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
-import java.io.Serializable;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.Map;
 
-import jakarta.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindException;
 
-@RestControllerAdvice
+@ControllerAdvice
 public class ApiExceptionHandler {
+	
+
 	// https://datatracker.ietf.org/doc/html/rfc7807
 	// Content-Type: application/problem+json
 	@JsonInclude(value = Include.NON_EMPTY)
@@ -119,31 +113,28 @@ public class ApiExceptionHandler {
 			}
 		}
 	}
-
 	@ExceptionHandler({ ErrorResponseException.class, HttpRequestMethodNotSupportedException.class })
 	public ProblemDetail defaultResponse(ErrorResponse exception) {
 		return exception.getBody();
 	}
 
-//	@ExceptionHandler({ NotFoundException.class })
-//	@ResponseStatus(HttpStatus.NOT_FOUND)
-//	public ErrorMessage notFoundRequest(Exception exception) {
-//		return new ErrorMessage(404, exception.getMessage(), ServletUriComponentsBuilder.fromCurrentRequest().build().toUriString(), null);
-//	}
 
 	@ExceptionHandler({ NotFoundException.class })
 	public ProblemDetail notFoundRequest(Exception exception) {
 		return ProblemDetail.forStatus(HttpStatus.NOT_FOUND);
 	}
 
-	@ExceptionHandler({ BadRequestException.class, DuplicateKeyException.class, HttpMessageNotReadableException.class })
+	@ExceptionHandler({ BadRequestException.class, DuplicateKeyException.class, HttpMessageNotReadableException.class,
+		MethodArgumentTypeMismatchException.class})
 	public ProblemDetail badRequest(Exception exception) {
 		return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, exception.getMessage());
 	}
+	
+	
 
 	@ExceptionHandler({ InvalidDataException.class, MethodArgumentNotValidException.class })
 	public ProblemDetail invalidData(Exception exception) {
-		var problem = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, "Datos invalidos");
+		var problem = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, "Invalid Data");
 		if (exception instanceof InvalidDataException ex && ex.hasErrors()) {
 //			ex.getErrors().forEach((n, v) -> problem.setProperty(n, v));
 			problem.setProperty("errors", ex.getErrors());
@@ -152,24 +143,15 @@ public class ApiExceptionHandler {
 			var errors = new HashMap<String, String>();
 			ex.getFieldErrors().forEach(item -> errors.put(item.getField(), item.getDefaultMessage()));
 			problem.setProperty("errors", errors);
-			problem = new ErrorMessage(400, "Datos invalidos","",errors);
+			problem = new ErrorMessage(400, "Invalid Data","",errors);
 		}
 		return problem;
 	}
 
-//	@ExceptionHandler({ AccessDeniedException.class, JWTVerificationException.class, FeignException.Forbidden.class })
-//	public ProblemDetail accessDenied(Exception exception) {
-//		return ProblemDetail.forStatusAndDetail(HttpStatus.FORBIDDEN, exception.getMessage());
-//	}
-//
-//	@ExceptionHandler({ FeignException.ServiceUnavailable.class, RetryableException.class })
-//	public ProblemDetail conflict(Exception exception) {
-//		return ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, exception.getMessage());
-//	}
-
-//	@ExceptionHandler({ Exception.class })
-//	public ProblemDetail unknow(Exception exception) {
-//		return ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, exception.getMessage());
-//	}
+	@ExceptionHandler({ Exception.class })
+	public ProblemDetail unknow(Exception exception) {
+		return ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, exception.getMessage());
+	}
 
 }
+
