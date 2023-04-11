381cb692ed246c1f70f4c7f5c54e2adeab7e4bce
xgervilla <xavier.gervilla@gmail.com>
2023-04-11 11:41:20 +0200

    Example of REST configuration with Spring

--- demo/src/main/java/com/example/domains/contracts/repositories/ActorRepository.java
+++ demo/src/main/java/com/example/domains/contracts/repositories/ActorRepository.java
@@ -7,13 +7,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.example.domains.core.repositories.contracts.RepositoryWithProjections;
 import com.example.domains.entities.Actor;
 import com.example.domains.entities.dtos.ActorDTO;
 import com.example.domains.entities.dtos.ActorShort;
 
-
+@RestResource(exported = false)
 public interface ActorRepository extends JpaRepository<Actor, Integer>, JpaSpecificationExecutor<Actor>, RepositoryWithProjections {
 	List<Actor> findTop5ByFirstNameStartingWithOrderByLastNameDesc(String prefijo);
 	List<Actor> findTop5ByFirstNameStartingWith(String prefijo, Sort orden);
